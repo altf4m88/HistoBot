c61d7b1f01fe63d8e4bcce83d617936bcfc07e0c
fadhilmauladhani <62723837+altf4m88@users.noreply.github.com>
2020-11-15 19:40:23 +0700

    refactor today-events code

--- commands/today-events.js
+++ commands/today-events.js
@@ -11,12 +11,18 @@ module.exports = {
         const pages = [];
         let fieldsArr = [];
 
-        const setField = (eventData, eventDate) => {
+        const setEmbedsMisc = (embed) => {
+            embed
+            .setTitle(`Today Events In History`)
+            .setColor('RED')
+            .setDescription(`\`Some Were Destined To Change The World, Brought To You By HistoBot ('-')7\``)
+        }
+
+        const setField = (eventData) => {
                 let limit = 10;
-                let tempBeds = new Discord.MessageEmbed()
-                .setTitle('Today Events In History')
-                .setColor('#FFF')
-                .setDescription(`\`Events That Shaped The World Today, Brought To You By HistoBot ('-')7\``)
+                let tempBeds = new Discord.MessageEmbed();
+                setEmbedsMisc(tempBeds);
+
 
                 for(let x = 0; x < eventData.length; x++){
                     tempBeds.addField(`${eventDate} ${eventData[x].year}`, `${eventData[x].text}`, false);
@@ -25,10 +31,9 @@ module.exports = {
                         fieldsArr.push(tempBeds.fields)
                         pages.push(tempBeds);
                         tempBeds.fields = []
-                        tempBeds = new Discord.MessageEmbed()
-                        .setTitle('Today Events In History')
-                        .setColor('#FFF')
-                        .setDescription(`\`Events That Shaped The World Today, Brought To You By HistoBot ('-')7\``)
+                        tempBeds = new Discord.MessageEmbed();
+                        setEmbedsMisc(tempBeds);
+
                         limit += 10;
                     } else if(limit >= eventData.length){
                         if(x == eventData.length - 1){
@@ -43,7 +48,7 @@ module.exports = {
         let date = today.getDate();
         let month = today.getMonth()+1;
         
-        console.log('im near the api')
+        
         fetch(`${APIURL}/date/${month}/${date}`)
         .then(response => response.json())
         .then(json => setField(json.data.Events, json.date))
