ab1012ad3bae46f58b880f226b470def2c13333a
fadhilmauladhani <62723837+altf4m88@users.noreply.github.com>
2020-11-16 18:51:27 +0700

    updated announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -10,22 +10,8 @@ module.exports = {
         .setTitle('🚨ANNOUNCEMENT DEAR COMRADES🚨')
         .setThumbnail('https://i.kym-cdn.com/photos/images/original/001/464/390/36d.jpg')
         .setDescription(`
-        Pembelajaran hari ini full penugasan pada progate.com
-        Silakan kalian akses progate.com
-        Yang belum punya akun silakan registrasi terlebih dahulu.
-        Pilih dan kerjakan tema berikut:
-        1. GIT
-        2. SQL Study 1+Latihan Soal pada GForm
-        3. Command Line
-
-        Setelah selesai mengerjakan kumpulkan hasilanya berupa ss 
-halaman akhir atau sertifikat penyelesaian tema tersebut
-pada classroom yang sudah disediakan, deadline hari ini jam 16.00.
-
-Cek kelengkapan absen dan tugas pada link:
-http://bit.ly/RekapAbsendanTugasBasdat
         `)
-        .setFooter(`Kaguya is currently offline, so i'll give the announcement ('-')/`)
+        .setFooter(``)
         .setColor('#d40000')
 
         message.channel.send(embed);
