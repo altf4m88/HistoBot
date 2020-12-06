d1d2ddbdbc5c64ed053b39561af0647d555ce607
altf4m88 <fadhilmauladhani@gmail.com>
2020-12-06 19:54:20 +0700

    update introduce.js message

--- commands/introduce.js
+++ commands/introduce.js
@@ -9,9 +9,9 @@ module.exports = {
         if(message.author.id !== "439976892343517184") return message.channel.send("Nope, you're not my master");
         const embed = new Discord.MessageEmbed()
         .setTitle('Let Me Introduce Myself')
-        .addField(`I am HistoBot`, `I'll provide you with daily dose of history knowledge. For now, i'm small and weak. But my master will lead me to glory and soon, i'll be able to restore the Soviet Union ☭`, false)
+        .addField(`I am HistoBot`, `I'll provide you with daily dose of history knowledge.`, false)
         .setFooter(`Historia magistra vitae est ('-')/`)
-        .setColor('#FFF')
+        .setColor('RANDOM')
 
         message.channel.send(embed)
     }
