1162266aa72b366c4b555be94e2aef6d75772f3b
altf4m88 <fadhilmauladhani@gmail.com>
2020-12-08 09:30:36 +0700

    set API request for covid-global command

--- commands/covid.js
+++ commands/covid.js
@@ -1,14 +1,27 @@
 const pagination = require('../node_modules/discord.js-pagination');
 const fetch = require("node-fetch");
-const APIURL = require("../config.json").BASEURL;
+const APIURL = require("../config.json").COVID_URL;
+const today = new Date;
 
 module.exports = {
-    name: 'covid',
-    description: 'Information for global new covid-19 cases',
-    usage:'|covid',
+    name: 'covid-global',
+    description: 'Give information for global new covid-19 cases',
+    usage:'|covid-global',
     example: '|covid',
-    execute(Discord, message, args){
+    execute(Discord, message){
         if(message.author.id !== "439976892343517184") return message.channel.send("Nope, you're not my master");
+        
+        fetch(`${APIURL}/summary`)
+        .then(response => response.json())
+        .then(json => {
+            
+
+
+            console.log(json.Global)
+        })
+        .catch(err => console.error(err));
+        ;
+
     }
 
 }
