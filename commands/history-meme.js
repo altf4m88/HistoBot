dbf8d94e99d99cfa4351930b668cefab2db37544
altf4m88 <fadhilmauladhani@gmail.com>
2020-12-17 20:55:20 +0700

    going to test history-meme command

--- commands/history-meme.js
+++ commands/history-meme.js
@@ -4,14 +4,28 @@ const APIURL = require("../config.json").MEME_URL;
 module.exports = {
     name: 'history-meme',
     description: 'Give random meme from /r/HistoryMemes',
-    usage:'|covid-global',
-    example: '|covid-global',
+    usage:'|history-meme',
+    example: '|history-meme',
     execute(Discord, message){
 
-
-
-
-
-
+        const embed = new Discord.MessageEmbed()
+        got('https://www.reddit.com/r/memes/random/.json')
+        .then(response => {
+            let content = JSON.parse(response.body);
+            console.log(content);
+            let permalink = content[0].data.children[0].data.permalink;
+            let memeUrl = `https://reddit.com${permalink}`;
+            let memeImage = content[0].data.children[0].data.url;
+            let memeTitle = content[0].data.children[0].data.title;
+            let memeUpvotes = content[0].data.children[0].data.ups;
+            let memeDownvotes = content[0].data.children[0].data.downs;
+            let memeNumComments = content[0].data.children[0].data.num_comments;
+            embed.setTitle(`${memeTitle}`)
+            embed.setURL(`${memeUrl}`)
+            embed.setImage(memeImage)
+            embed.setColor('RANDOM')
+            embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
+            message.channel.send(embed);
+        })
     }
 }
