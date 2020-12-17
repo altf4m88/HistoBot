2162697d9e8e15d66794e8a0224357d80b6963f9
altf4m88 <fadhilmauladhani@gmail.com>
2020-12-17 20:51:00 +0700

    create new command history-meme.js

---
+++ commands/history-meme.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+const APIURL = require("../config.json").MEME_URL;
+
+module.exports = {
+    name: 'history-meme',
+    description: 'Give random meme from /r/HistoryMemes',
+    usage:'|covid-global',
+    example: '|covid-global',
+    execute(Discord, message){
+
+
+
+
+
+
+    }
+}
