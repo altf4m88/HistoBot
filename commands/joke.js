573f2565101a8eef66093ddb8df9cace8a428655
altf4m88 <fadhilmauladhani@gmail.com>
2020-12-23 19:18:02 +0700

    update joke.js, wrote the main code

--- commands/joke.js
+++ commands/joke.js
@@ -4,21 +4,39 @@ const APIURL = require("../config.json").JOKES_URL;
 module.exports = {
     name: 'joke',
     description: 'Give a random joke, you can select the category (random, general, programming)',
-    usage: '|joke <random | general | programming>',
+    usage: '|joke <random | general | programming | knock-knock>',
     example: '|joke programming',
     execute(Discord, message, args){
         let embed = new Discord.MessageEmbed();
         let type = args[0];
-        
-        https://official-joke-api.appspot.com/jokes/programming/random
 
         if(type){
-            fetch(``)
+            fetch(`${APIURL}/jokes/${type}/random`)
+            .then(response => response.json())
+            .then(json => {
+                let joke = json[0];
 
+                embed.setTitle("Jokes")
+                .setColor('RANDOM')
+                .addField(`${joke.setup}` , ` \`\`\` *${joke.punchline}* \`\`\`\ `, false)
+                .setFooter("I hope that's a good one");
 
+                return message.channel.send(embed)
+            })
+            .catch(err => message.channel.send("no category found"));
+        } else {
+            fetch(`${APIURL}/random_joke`)
+            .then(response => response.json())
+            .then(joke => {
+                embed.setTitle("Jokes")
+                .setColor('RANDOM')
+                .addField(`${joke.setup}` , ` \`\`\` *${joke.punchline}* \`\`\`\ `, false)
+                .setFooter("You can select jokes from this category: General, Programming, Knock-Knock");
+
+                return message.channel.send(embed)
+            })
+            .catch(err => message.channel.send("the api did not respond"));
         }
 
-
-        message.channel.send(embed)
     }
 }
